391cfc21d366cb6e3505bd952128904665b72eb7
gngpp <gngppz@gmail.com>
2026-06-15 13:09:02 +0800

    fmt

--- h3/src/server/connection.rs
+++ h3/src/server/connection.rs
@@ -131,9 +131,11 @@ where
                 return Ok(None);
             }
         };
+
+        let resolver = self.create_resolver_internal(stream);
+
         // send the grease frame only once
         self.inner.send_grease_frame = false;
-        let resolver = self.create_resolver_internal(stream);
 
         Ok(Some(resolver))
     }
