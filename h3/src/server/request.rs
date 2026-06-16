f64cc0afc95b9960178b72db7cf83fdb2d89a7d5
gngpp <gngppz@gmail.com>
2026-06-16 14:30:53 +0800

    fmt

--- h3/src/server/request.rs
+++ h3/src/server/request.rs
@@ -19,7 +19,7 @@ use crate::{
         frame::{Frame, PayloadLen},
         headers::Header,
     },
-    qpack::{self},
+    qpack,
     quic::{self, SendStream, StreamId},
     shared_state::{ConnectionState, SharedState},
 };
