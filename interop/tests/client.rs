38f02bd43e0052a911305a4074b7d96cfece2f19
0x676e67 <gngppz@gmail.com>
2026-06-25 10:41:27 +0800

    test(http3, client): add real GREASE interop tests (#25)
    * test(http3, client): add HTTP/3 client interop tests
    
    * test(http3, client): add real-server interop tests
    
    * Fix tests
    
    * test(http3, client): add GREASE interop tests

--- interop/tests/client.rs
+++ interop/tests/client.rs
@@ -112,14 +112,44 @@ async fn tquic_qpack_client_on_server_on() -> Result<(), BoxError> {
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 4)]
 #[ignore = "real HTTP/3 interop tests need public network access"]
-async fn public_servers_qpack_stateless() -> Result<(), BoxError> {
-    support::public::run_client_interop("qpack-stateless", ClientInteropConfig::stateless_qpack())
-        .await
+async fn public_servers_qpack_stateless_grease_off() -> Result<(), BoxError> {
+    support::public::run_client_interop(
+        "qpack-stateless",
+        ClientInteropConfig::stateless_qpack(),
+        support::public::GreaseMode::OFF,
+    )
+    .await
 }
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 4)]
 #[ignore = "real HTTP/3 interop tests need public network access"]
-async fn public_servers_qpack_dynamic() -> Result<(), BoxError> {
-    support::public::run_client_interop("qpack-dynamic", ClientInteropConfig::qpack_dynamic_table())
-        .await
+async fn public_servers_qpack_stateless_grease_on() -> Result<(), BoxError> {
+    support::public::run_client_interop(
+        "qpack-stateless",
+        ClientInteropConfig::stateless_qpack(),
+        support::public::GreaseMode::ON,
+    )
+    .await
+}
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 4)]
+#[ignore = "real HTTP/3 interop tests need public network access"]
+async fn public_servers_qpack_dynamic_grease_off() -> Result<(), BoxError> {
+    support::public::run_client_interop(
+        "qpack-dynamic",
+        ClientInteropConfig::qpack_dynamic_table(),
+        support::public::GreaseMode::OFF,
+    )
+    .await
+}
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 4)]
+#[ignore = "real HTTP/3 interop tests need public network access"]
+async fn public_servers_qpack_dynamic_grease_on() -> Result<(), BoxError> {
+    support::public::run_client_interop(
+        "qpack-dynamic",
+        ClientInteropConfig::qpack_dynamic_table(),
+        support::public::GreaseMode::ON,
+    )
+    .await
 }
